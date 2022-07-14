e2e2a6ef6128f236c88114dd05f8472cb56e69cc
Edgar Mendez <edgar.ym26@gmail.com>
2022-07-14 01:28:28 -0400

    starting form for contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
+
 
 export default function Contact() {
   return (
@@ -20,3 +22,28 @@ export default function Contact() {
     </div>
   );
 }
+
+function Form() {
+  // Create state variables for the fields in the form
+  // We are also setting their initial values to an empty string
+  const [email, setEmail] = useState('');
+  const [userName, setUserName] = useState('');
+  // TODO: Create a password variable and a function "setPassword" using useState
+
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    // TODO: Add an else statement to the end that will set the password to the value of 'inputValue'
+
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'userName') {
+      setUserName(inputValue);
+    }
+  }};
